663efa2433610f5953aac91cd326dac1ed2a5470
Niloy Kumar Kundu <47324091+NiloyKumarKundu@users.noreply.github.com>
2020-03-19 08:47:33 +0600

    Create NextGreaterElement.cpp

---
+++ Next Greater Element/Iterative Approach/NextGreaterElement.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void NextGreaterElement(int *arr, int n) {
+    int next;
+    for(int i = 0; i < n; i++) {
+        next = -1;
+        for(int j = 0; j < n; j++) {
+            if(arr[i] < arr[j]) {
+                next = arr[j];
+                break;
+            }
+        }
+        cout << arr[i] << " --- " << next << endl;
+    }
+}
+
+
+int main() {
+    int n;
+    cin >> n;
+    int a[n];
+    for(int i = 0; i < n; i++) cin >> a[i];
+
+    NextGreaterElement(a, n);
+}
