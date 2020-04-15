641d211f96536113df8e71320a7f2a671a4ea866
Niloy Kumar Kundu <47324091+NiloyKumarKundu@users.noreply.github.com>
2020-04-15 13:52:10 +0600

    Data Structures and Algorithm

--- Graph/Cycle Detecting/Undirected Graph/detectCycle.cpp
+++ Graph/Cycle Detecting/Undirected Graph/detectCycle.cpp
@@ -1,40 +1,54 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
+#define max 10000000 + 5
+#define mod 10000000 + 5
+#define pb push_back
+#define pairs pair<int, int>
+#define vi vector<int>
+#define vb vector<bool>
+#define vii vector<pairs>
+#define lb lower_bound
+#define ub upper_bound
+#define lli long long int
+#define endl '\n'
 using namespace std;
-vector<int> vc[10000];
-vector<bool> visited;
-bool result = false;
 
+vi graph[max];
+bool visited[max];
 
-bool dfs(int v, int parent) {
-    visited[v] = true;
-
-    for(int i : vc[v]) {
-        if(!visited[i]) {
-            dfs(i, v);
-        } else if(visited[i] && i != parent) {
-            return result = true;
+bool dfs(int node, int parent) {
+    visited[node] = true;
+    for(int child : graph[node]) {
+        if(!visited[child]) {
+            if(dfs(child, node)) {
+                return true;
+            }
+        } else if(child != parent) {
+            return true;
         }
     }
-    return result = false;
+    return false;
 }
 
-int main () {
-    int V, E;
-    cin >> V >> E;
-    visited.assign(V, false);
-    for(int i = 0; i < E; i++) {
-        int x, y;
-        cin >> x >> y;
-        vc[x].push_back(y);
-        vc[y].push_back(x);
-    }
+int main() {
+    #ifndef Niloy
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
 
-    bool ans = dfs(0, -1);
-    if(ans) {
-        cout << "Cycle Found!" << endl;
-    } else {
-        cout << "Cycle Not Found" << endl;
-    }
-    
-    return 0;
+        int v, e, a, b;
+        cin >> v >> e;
+        for (int i = 0; i < e; i++) {
+            cin >> a >> b;
+            graph[a].pb(b);
+            graph[b].pb(a);
+        }
+
+        bool result = dfs(1, 1);
+        if(result) {
+            cout << "Cycle Found!" << endl;
+        } else {
+            cout << "No Cycle Found!" << endl;
+        }
+
+        return 0;
 }
