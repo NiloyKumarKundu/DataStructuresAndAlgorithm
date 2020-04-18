3f112dfae0552fdafb7303f83efc79be72ca637e
Niloy Kumar Kundu <47324091+NiloyKumarKundu@users.noreply.github.com>
2020-04-18 16:49:42 +0600

    Data Structures and Algorithm

--- Graph/Graph Traversal/BFS/bfs.cpp
+++ Graph/Graph Traversal/BFS/bfs.cpp
@@ -1,43 +1,62 @@
 /// Time Complexity O(vertex + edges)
 
 #include <bits/stdc++.h>
-#define MAX 1000
+#define max 10000000 + 5
+#define mod 10000000 + 5
+#define pb push_back
+#define pairs pair<int, int>
+#define vi vector<int>
+#define vb vector<bool>
+#define vii vector<pairs>
+#define lb lower_bound
+#define ub upper_bound
+#define lli long long int
+#define endl '\n'
 using namespace std;
 
-vector<bool> visited;
-vector<int> graph[MAX];
+vi graph[max];
+bool visited[max];
+int dis[max];
+queue<int> que;
 
-void bfs(int u)
-{
-    queue<int> q;
-    q.push(u);
-    visited[u] = true;
+void bfs(int node) {
+    visited[node] = true;
+    dis[node] = 0;
+    que.push(node);
 
-    while (!q.empty()) {
-        int f = q.front();
-        q.pop();
-        cout << f << " ";
-        for(auto i : graph[f]) {
-            if(!visited[i]) {
-                q.push(i);
-                visited[i] = true;
+    while(!que.empty()) {
+        int cur = que.front();
+        que.pop();
+        cout << cur << " ";
+        for (int child : graph[cur]) {
+            if(!visited[child]) {
+                que.push(child);
+                dis[child] = dis[cur] + 1;
+                visited[child] = true;
             }
         }
     }
 }
 
-int main()
-{
-    int n, e;
-    cin >> n >> e;
-    visited.assign(n, false);
-    int a, b;
+int main() {
+#ifndef Niloy
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+
+    int v, e, a, b;
+
+    cin >> v >> e;
     for (int i = 0; i < e; i++) {
         cin >> a >> b;
-        graph[a].push_back(b);
-        graph[b].push_back(a);
+        graph[a].pb(b);
+        graph[b].pb(a);
     }
     bfs(1);
+    cout << endl;
+
+    for (int i = 1; i <= v; i++)
+        cout << dis[i] << endl;
 
     return 0;
 }
