94cd9628b18d50c7a430130d67ddb075ef29b2bd
Niloy Kumar Kundu <47324091+NiloyKumarKundu@users.noreply.github.com>
2020-05-12 13:54:25 +0600

    Data Structures and Algorithm

--- Number Theory/Sieve of Eratosthenes/sieveOfEratosthenes.cpp
+++ Number Theory/Sieve of Eratosthenes/sieveOfEratosthenes.cpp
@@ -1,5 +1,6 @@
 #include <bits/stdc++.h>
-#define max 1000000
+#define max 90000000
+#define maxN 1000000
 #define mod 10000000 + 5
 #define pb push_back
 #define pairs pair<int, int>
@@ -12,28 +13,14 @@
 #define endl '\n'
 using namespace std;
 
-int isPrime[max];
-
-bool getPrimes(int n) {
-    if(n < 2)
-        return false;
-    if(n == 2)
-        return true;
-    if(n % 2 == 0)
-        return false;
-    return isPrime[n] == 1;
-}
+bool isPrime[max];
 
 void sieve() {
-    for (int i = 1; i <= max; i++) {
-        isPrime[i] = 1;
-    }
-
-    isPrime[0] = isPrime[1] = 0;
-
-    for (int i = 3; i * i <= max; i++) {
+    memset(isPrime, true, sizeof(isPrime));
+    isPrime[0] = 0, isPrime[1] = 0;
+    for (int i = 2; i * i <= max; i++) {
         if(isPrime[i]) {
-            for (int j = i * i; j <= max; j += i + i) {
+            for (int j = i * i; j <= max; j += i) {
                 isPrime[j] = 0;
             }
         }
@@ -45,19 +32,14 @@ int main() {
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    int t;
-    cin >> t;
     sieve();
-    while(t--) {
-        int n;
-        cin >> n;
-        if(getPrimes(n)) {
-            cout << "Prime" << endl;
-        } else {
-            cout << "Not Prime!" << endl;
-        }
+    int limit = 40;         // Limit joto porjonto lagbe
+    for (int i = 1; i < limit; i++) {
+        cout << isPrime[i] << endl;
     }
-
+    
     return 0;
 }
