9184202c369ba800adf38b1830e4ce9cd7fbc52b
Niloy Kumar Kundu <47324091+NiloyKumarKundu@users.noreply.github.com>
2020-05-24 13:19:04 +0600

    Data Structures and Algorithm

--- Number Theory/Algorithms/Sieve of Eratosthenes/sieveOfEratosthenes (More Optimized).cpp
+++ Number Theory/Algorithms/Sieve of Eratosthenes/sieveOfEratosthenes (More Optimized).cpp
@@ -29,9 +29,8 @@ void sieve() {
         isPrime[i] = true;
         // Primes.push_back(0);
     }
-	isPrime[0] = isPrime[1] = false;
 
-	for (int i = 2; i * i <= max; i++) {
+    for (int i = 3; i * i <= max; i += 2) {
         if (isPrime[i]) {
             for (int j = i * i; j <= max; j += i)
                 isPrime[j] = false;
@@ -48,7 +47,7 @@ void sieve() {
 
 
 int main() {
-#ifdef Niloy
+#ifndef Niloy
     freopen("input.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 #endif
@@ -56,8 +55,8 @@ int main() {
     cin.tie(NULL);
     
     sieve();
-    for (int i = 1; i < 200; i++) {
-        cout << isPrime[i] << endl;
+    for (int i = 0; i < 200; i++) {
+        cout << Primes[i] << endl;
     }
     
     return 0;
