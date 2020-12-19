4d596dcb93fc48c0fc3962ee20cfcc28d9ca60b4
Niloy <nkundu191071@gmail.com>
2020-12-19 16:25:04 +0600

    Data Structure and Algorithm

--- Dynamic Programming/Coin Change Problem/NumberOfCombinations.cpp
+++ Dynamic Programming/Coin Change Problem/NumberOfCombinations.cpp
@@ -165,3 +165,56 @@ int32_t main() {
 	showTime;
 	return 0;
 }
+
+
+
+
+
+// LeedCode Submission Code:
+/*
+
+
+class Solution {
+public:
+    int change(int amount, vector<int>& coins) {
+        int n = coins.size();
+        vector<vector<int>> dp(n + 1, vector<int>(amount + 1));
+        if(amount == 0) {
+            return 1;
+        }
+        if(amount > 0 && coins.size() == 0) {
+            return 0;
+        }
+        
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j <= amount; j++) {
+                if(j == 0) {
+                    dp[i][j] = 1;
+                } else if(j % coins[0] == 0 && i == 0) {
+                    dp[i][j] = 1;
+                } else {
+                    dp[i][j] = 0;
+                }
+            }
+        }
+        
+        for(int i = 1; i < n; i++) {
+            for(int j = 1; j <= amount; j++) {
+                if(coins[i] > j) {
+                    dp[i][j] = dp[i - 1][j];
+                } else {
+                    dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i]];
+                }
+            }
+        }
+        
+        
+        return dp[n - 1][amount];
+    }
+};
+
+
+
+*/
+
+
